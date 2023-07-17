06815690bd8d09be95fa73cfb979668aafc31e87
MauriDiPietro <dipietro.jm@gmail.com>
2023-07-17 09:52:17 -0300

    render products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './components/product/models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-app';
+  http = inject(HttpClient);
+  products: Product[] = [];
+  
+  changeTitle() {
+    this.title = 'changed'
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+      })
+  }
 }
+
